a0bc9b022f5b18b7cd958286dc8a48213481ffcd
estebanstack <130319203+estebanstack@users.noreply.github.com>
2025-10-27 14:50:13 -0500

    Update argument handling and default output file

--- ProyectoSintactico/main.py
+++ ProyectoSintactico/main.py
@@ -2,9 +2,19 @@ import sys
 from parser import analizar_archivo
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Uso: python main.py <entrada.py> <salida.txt>")
+    if len(sys.argv) < 2:
+        print("Uso: python main.py <archivo_entrada.py>")
         sys.exit(1)
+
     ruta_entrada = sys.argv[1]
-    ruta_salida = sys.argv[2]
+    ruta_salida = "salida.txt"  
+
+    print(f"Analizando '{ruta_entrada}'... El resultado se guardara en '{ruta_salida}'")
+
+
     analizar_archivo(ruta_entrada, ruta_salida)
+
+    
+    with open(ruta_salida, "r", encoding="utf-8") as f:
+        print("\n--- Resultado del analisis ---")
+        print(f.read())
